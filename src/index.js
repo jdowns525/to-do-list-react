36b680a38865f02eb910e359befd8cef928b93c0
Josseph Downs <downsjosseph@gmail.com>
2023-06-15 15:17:14 +0000

    added styling to to do list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div className="container">
+    <header className="header">
+      <h1 className="title">Cool Todo List</h1>
+    </header>
+    <main className="main">
+      <App />
+    </main>
+    <footer className="footer">
+      <p className="footer-text">Powered by Josseph Downs</p>
+    </footer>
+  </div>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
